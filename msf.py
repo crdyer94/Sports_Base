c1e40bb6de3e752173f52634352ed0314e7ec3c2
Camille Dyer <crdyer94@gmail.com>
2019-05-24 23:02:15 +0000

    Added team data to display on page

--- msf.py
+++ msf.py
@@ -7,8 +7,7 @@ from requests.auth import HTTPBasicAuth
 MYSPORTSFEED_TOKEN = os.environ.get('MYSPORTSFEED_TOKEN')
 MYSPORTSFEED_PASS = os.environ.get('MYSPORTSFEED_PASS')
 
-SPORTSFEED_URL = "https://api.mysportsfeeds.com/v2.1/pull/nfl/players.json"
-
+SPORTSFEED_URL = "https://api.mysportsfeeds.com/v2.1/pull/nfl/"
 
 
 def get_search_results(playername):
@@ -16,22 +15,22 @@ def get_search_results(playername):
 
     Authorization: Basic [MYSPORTSFEED_TOKEN + ":" + MYSPORTSFEED_PASS]
 
-    response = requests.get(SPORTSFEED_URL + f"?player={playername}",
+    response = requests.get(SPORTSFEED_URL + f"players.json?player={playername}",
          auth=HTTPBasicAuth(MYSPORTSFEED_TOKEN, MYSPORTSFEED_PASS))
     response=response.json()
+
     response_display = []
+
     for obj in response["players"]:
+
         obj = obj["player"]
+
         fullname = obj.get("firstName") + " " + obj.get("lastName")
         athlete_id = obj["id"]
+
         athlete_route = {athlete_id:fullname}
 
-
         response_display.append(athlete_route)
-
-
-    # print(MYSPORTSFEED_PASS)
-    # print (response.json())
 
     return response_display
 
@@ -51,12 +50,17 @@ def get_athlete_info(athlete_id):
 
     Authorization: Basic [MYSPORTSFEED_TOKEN + ":" + MYSPORTSFEED_PASS]
 
-    response = requests.get(SPORTSFEED_URL + f"?player={athlete_id}",
+    response = requests.get(SPORTSFEED_URL + f"players.json?player={athlete_id}",
          auth=HTTPBasicAuth(MYSPORTSFEED_TOKEN, MYSPORTSFEED_PASS))
     response=response.json()
 
     response_players_dict = response.get("players")[0]
     player_dict = response_players_dict.get("player")
+
+    response_team_dict = response.get("references")
+    team_dict = response_team_dict.get("teamReferences")[0]
+    arena = team_dict.get("homeVenue")
+
     player_profile = {
     "fullname": player_dict.get("firstName") + " " +
                 player_dict.get("lastName"), 
@@ -67,55 +71,21 @@ def get_athlete_info(athlete_id):
     "age": player_dict.get("age"),
     "highschool": player_dict.get("highSchool"),
     "college": player_dict.get("college"),
-    "rosterpic": player_dict.get("officialImageSrc")
+    "rosterpic": player_dict.get("officialImageSrc"),
+    "current_team": team_dict.get("city") + " " + team_dict.get("name"),
+    "team_abbr": team_dict.get("abbreviation"),
+    "jersey_num": player_dict.get("jerseyNumber"), 
+    "arena": arena.get("name")
 
+    # "rosterstatus": player_dict.get("currentRosterStatus"),
+    
     }
-
 
 
     return player_profile
 
- 
 
-
-
-
-
-# Team Stuff
-#  player_dict{'jerseyNumber': 12, 'currentTeam': {'id': 50, 'abbreviation': 'NE'}, 
-#  player_dict{'teamAsOfDate': {'id': 50, 'abbreviation': 'NE'}
-
-
-# Player stuff to loop specifically
-# # 'currentRosterStatus': 'ROSTER', 'currentInjury': None,
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# # Team Stuff
+# #  player_dict{'jerseyNumber': 12, 'currentTeam': {'id': 50, 'abbreviation': 'NE'}, 
+# #  player_dict{'teamAsOfDate': {'id': 50, 'abbreviation': 'NE'}
 
