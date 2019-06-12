102dc75c849196d22f18198ce602502753303123
Camille Dyer <crdyer94@gmail.com>
2019-06-12 07:59:33 +0000

    Working on the Athlete Profile UI

--- static/JS/athlete_info.js
+++ static/JS/athlete_info.js
@@ -1,36 +1,29 @@
 
-function showBioInfo() {
-  var x = document.getElementById("athletebio");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
 function showStats() {
   var x = document.getElementById("athletestats");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
+  if (x.style.display === "none") {
     x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
 
 function showTweets() {
   var x = document.getElementById("athletetweets");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
+   if (x.style.display === "none") {
     x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
 
 function showArrests() {
   var x = document.getElementById("athletearrests");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
+  if (x.style.display === "none") {
     x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
 }
+
+
